39dc465f4df6ed171226631d925f13b12ac8b2da
JuGonca <julia.g0.rocga@gmail.com>
2025-11-15 19:37:30 -0300

    Deletando projeto

--- ProjetoFinal/js/login-script.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector(".login-form"); //"pega" o formulário
-
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-});
